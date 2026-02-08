42dff565bafddefe4e5a382224e86cb77097168e
maaz <maazk2414@gmail.com>
2026-02-08 10:25:04 +0530

    string problem1

---
+++ day1_string_basic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int main(){
+    // int n;
+    // cin>>n;
+
+    // char arr[n];
+    // cin>>arr;
+    // cout<<arr;
+    // cout<<arr[3];
+    string s;
+    getline(cin,s);
+    cout<<s;
+}
---
+++ defanging_an_ip_address.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+string ip_address(string str){
+    string ans = "";
+   
+    for(int i=0;i<str.length();i++){
+        if(str[i] == '.'){
+            ans+="[.]";
+        }
+        else{
+            ans.push_back(str[i]);
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    string str;
+    getline(cin,str);
+    cout<<ip_address(str);
+
+}
